95498565a5d50ea97fbe5246d350522d07db8cdc
Roopa Reddy <Roopa Reddy@LAPTOP-ATVR3R4K>
2022-12-28 13:34:05 +0530

    build666

---
+++ selenium/src/Actions/implicitwait.java
@@ -0,0 +1,21 @@
+package Actions;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class implicitwait
+{
+public static void main(String[] args) 
+{
+	System.setProperty("webdriver.gecko.driver","./soft/geckodriver.exe");
+	WebDriver  driver=new FirefoxDriver();
+	driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	driver.get("http://localhost/login.do");
+	driver.findElement(By.id("username")).sendKeys("admin");
+	driver.findElement(By.name("pwd")).sendKeys("manager");	
+	driver.findElement(By.xpath("//div[.='Login ']")).click();		
+}
+}
